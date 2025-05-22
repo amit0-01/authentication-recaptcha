c0b425346eb6620ff65f20638405223551b12f74
amit0-01 <your-email@example.com>
2025-05-22 23:33:10 +0530

    minor changes

--- db.js
+++ db.js
@@ -4,4 +4,13 @@ dotenv.config();
 
 const pool = new Pool({ connectionString: process.env.DB_URL });
 
+pool.connect()
+  .then(client => {
+    console.log('✅ Database connected successfully.');
+    client.release(); 
+  })
+  .catch(err => {
+    console.error('❌ Database connection failed:', err.stack);
+  });
+
 module.exports = pool;
